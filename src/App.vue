35e0ccb6f5adab629bf9d2586c1a79bf7774cf3f
Chen Dar-Yen <chendaryen@outlook.com>
2019-04-18 14:29:11 +0800

    change the appearance of login page

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,13 @@
 <script>
 
 export default {
-  name: 'App',
-  created (){
-    // const api= `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
-    // this.$http.get(api).then((response) => {
-    //   console.log(response.data)
-    // })
-  }
-  // components: {
-  //   HelloWorld
-  // }
+  name: 'App'
 }
 </script>
 
 <style lang="scss">
 @import "./assets/all";
+#app {
+  height: 100%;
+}
 </style>
